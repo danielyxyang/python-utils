9bb01675a07f8681997ce21dd0d4d94ee2f704da
Daniel Yang <44120873+danielyxyang@users.noreply.github.com>
2023-06-24 17:25:55 +0200

    [math] NEW - added MixtureDistribution

--- math/__init__.py
+++ math/__init__.py
@@ -37,3 +37,25 @@ def intersect_functions(f1, f2, mode="left"):
     else:
         print("WARNING: unknown mode for intersecting two functions")
     return np.nonzero(np.diff(np.sign(f1 - f2)))[0] + offset
+
+
+class MixtureDistribution():
+    def __init__(self, distributions):
+        self.ps = np.array([p for p, _ in distributions])
+        self.dists = [d for _, d in distributions]
+    
+    def sample(self, size=None):
+        dist_i = np.random.choice(np.arange(len(self.dists)), size=size, p=self.ps)
+        dist_rvs = np.array([dist.rvs(size) for dist in self.dists])
+        samples = dist_rvs[dist_i, np.arange(size)]
+        return samples
+    
+    def pdf(self, x):
+        dist_pdfs = np.array([dist.pdf(x) for dist in self.dists])
+        pdf = self.ps @ dist_pdfs
+        return pdf
+    
+    def mean(self):
+        dist_means = np.array([dist.mean() for dist in self.dists])
+        mean = self.ps @ dist_means
+        return mean

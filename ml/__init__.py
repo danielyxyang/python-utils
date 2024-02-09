5d1514183a66dd515446bf6f5e28a44a7683ba2c
Daniel Yang <44120873+danielyxyang@users.noreply.github.com>
2024-02-09 12:09:49 +0100

    [ml] added TorchMemoryProfiler

--- ml/__init__.py
+++ ml/__init__.py
@@ -1,1 +1,2 @@
+from .profiler import *
 from .reproducibility import *
---
+++ ml/profiler.py
@@ -0,0 +1,79 @@
+__all__ = ["TorchMemoryProfiler"]
+
+import gc
+import logging
+import os
+import pickle
+
+try:
+    import torch
+    from torch.cuda._memory_viz import trace_plot
+    _IMPORTED_TORCH = True
+except ImportError:
+    _IMPORTED_TORCH = False
+
+logger = logging.getLogger(__name__)
+
+class TorchMemoryProfiler():
+    """Class for profiling GPU memory usage by PyTorch."""
+
+    def __init__(self, reset_peak_stats=False, record_mem_history=False):
+        self.reset_peak_stats = reset_peak_stats
+        self.record_mem_history = record_mem_history
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, *exc):
+        self.stop()
+
+    def start(self):
+        gc.collect()
+        if self.reset_peak_stats:
+            torch.cuda.memory.reset_peak_memory_stats()
+        if self.record_mem_history:
+            TorchMemoryProfiler.record_memory_history()
+
+    def stop(self):
+        self.gpu_mem_stats = torch.cuda.memory.memory_stats()
+        if self.record_mem_history:
+            self.gpu_mem_history = torch.cuda.memory._snapshot()
+
+    def save(self, path="memory_history"):
+        if self.record_mem_history:
+            TorchMemoryProfiler.save_memory_history(path=path, snapshot=self.gpu_mem_history)
+
+    @property
+    def gpu_peak_memory(self):
+        return self.gpu_mem_stats.get("allocated_bytes.all.peak", 0)
+
+    @property
+    def gpu_peak_memory_reserved(self):
+        return self.gpu_mem_stats.get("reserved_bytes.all.peak", 0)
+
+    @staticmethod
+    def record_memory_history():
+        if torch.cuda.is_available():
+            if hasattr(torch.cuda.memory, "_dump_snapshot"):
+                # start recording memory history
+                torch.cuda.memory._record_memory_history()
+            else:
+                logger.warning("Recording memory history requires torch >= 2.1.")
+
+    @staticmethod
+    def save_memory_history(path="memory_history", snapshot=None):
+        if torch.cuda.is_available():
+            if hasattr(torch.cuda.memory, "_dump_snapshot"):
+                # create snapshot of memory history
+                if snapshot is None:
+                    snapshot = torch.cuda.memory._snapshot()
+                # save snapshot of memory history as pickle and HTML
+                if os.path.dirname(path) != "":
+                    os.makedirs(os.path.dirname(path), exist_ok=True)
+                with open(f"{path}.pickle", "wb") as file:
+                    pickle.dump(snapshot, file)
+                with open(f"{path}.html", "w") as file:
+                    file.write(trace_plot(snapshot))
+            else:
+                logger.warning("Saving memory history requires torch >= 2.1.")

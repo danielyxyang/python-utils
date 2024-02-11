d72e1aebcd9b9a227efdc0f90c0b3b0d682acab5
Daniel Yang <44120873+danielyxyang@users.noreply.github.com>
2024-02-11 22:07:12 +0100

    [tools] added ability to configure other loggers with setup_logging

--- tools/logging.py
+++ tools/logging.py
@@ -8,14 +8,28 @@ import logging
 import sys
 
 
-def setup_logging():
+def setup_logging(level=logging.INFO, format=None, dateformat=None, other_loggers=[]):
+    if format is None:
+        format="[%(asctime)s] %(levelname)-8s %(name)s:%(lineno)-5d %(message)s"
+    if dateformat is None:
+        dateformat="%Y-%m-%d %H:%M:%S"
+
+    # configure logging system
     logging.basicConfig(
         stream=sys.stdout, # log to stdout instead of stderr to sync with print()
-        level=logging.INFO,
-        format="[%(asctime)s] %(levelname)-8s %(name)s:%(lineno)-5d %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
+        level=level,
+        format=format,
+        datefmt=dateformat,
     )
     logging.captureWarnings(True)
+
+    # configure other loggers
+    for logger in other_loggers:
+        if isinstance(logger, str):
+            logger = logging.getLogger(logger)
+        for handler in logger.handlers:
+            handler.setFormatter(logging.Formatter(format, dateformat))
+            handler.setLevel(level)
 
 # reference: https://gist.github.com/simon-weber/7853144
 @contextlib.contextmanager

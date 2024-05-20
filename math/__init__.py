93aa547c9bcd6b12d5528c2875e89e5341dd5508
Daniel Yang <44120873+danielyxyang@users.noreply.github.com>
2024-05-20 14:56:53 +0900

    [math] added safe_div

--- math/__init__.py
+++ math/__init__.py
@@ -1,3 +1,4 @@
 from .analysis import *
+from .basics import *
 from .set_theory import *
 from .statistics import *
---
+++ math/basics.py
@@ -0,0 +1,6 @@
+__all__ = ["safe_div"]
+
+import numpy as np
+
+def safe_div(a, b, default=0):
+    return np.divide(a, b, out=np.full_like(a, default, dtype=float), where=b != 0)

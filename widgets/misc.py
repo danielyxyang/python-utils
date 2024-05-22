10630b47509cd78be580891137f09179f733cbcc
Daniel Yang <44120873+danielyxyang@users.noreply.github.com>
2024-05-22 13:13:39 +0900

    [widgets] added csv mode to display_table

--- widgets/misc.py
+++ widgets/misc.py
@@ -5,26 +5,28 @@ from IPython.display import display
 
 logger = logging.getLogger(__name__)
 
-def display_table(table, sort=None, columns=None, html=True, align=None):
+def display_table(table, sort=None, columns=None, mode="html", align=None):
     df = pd.DataFrame(table)
     if sort is not None:
         df = df.sort_values(sort)
     if columns is not None:
         df = df[columns]
-    if html:
+    if mode == "html":
         df_style = df.style
     if align is not None:
-        if html:
+        if mode == "html":
             df_style = df_style.set_properties(**{"text-align": align}).set_table_styles([dict(selector="th", props=[("text-align", align)])])
         else:
-            logger.warning("Aligning columns is not supported in text-mode.")
+            logger.warning("Aligning columns is only supported in mode \"html\".")
     with pd.option_context(
         "display.expand_frame_repr", False,
         "display.max_rows", None,
         "display.max_columns", None,
         "display.max_colwidth", None,
     ):
-        if html:
+        if mode == "html":
             display(df_style)
+        elif mode == "csv":
+            print(df.to_csv(index=False, sep="\t"))
         else:
             print(df)

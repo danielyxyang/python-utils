8120aad8f33cb552bb48503424fa6833a801df34
Daniel Yang <44120873+danielyxyang@users.noreply.github.com>
2024-11-22 15:54:43 +0100

    [tools] added docstring for parse_logs

--- tools/logging.py
+++ tools/logging.py
@@ -39,6 +39,41 @@ def logging_disabled(level=logging.CRITICAL):
         logging.disable(prev_root_level)
 
 def parse_logs(path, patterns, repeat=False):
+    """Parse log files based on the given list of patterns.
+
+    Examples:
+        Parsing basic log file
+        ```
+        log = parse_logs("path/to/file.log", {
+            "int":       r"An integer:  (\d+)",
+            "float":     r"A float:     (\d+(?:\.\d*)?)",
+            "duration":  r"A duration:  ([0-9\:]+)",
+            "timestamp": r"A timestamp: ([0-9\:\- ]+)",
+        })
+        log["duration"] = datetime.strptime(log["duration"], "%H:%M:%S") - datetime(1900, 1, 1)
+        log["timestamp"] = datetime.strptime(log["timestamp"], "%Y-%m-%d %H:%M:%S")
+        ```
+
+        Parsing multiple log files
+        ```
+        from utils_ext.widgets import display_table
+
+        logs = [parse_logs(path, ...) for path in sorted(glob.glob("path/to/*.log"))]
+        logs = pd.DataFrame(logs)
+        display_table(logs_grouped)
+        ```
+
+    Args:
+        path (str): The path to the log file.
+        patterns (dict): The dictionary mapping pattern name to a regex string.
+        repeat (bool, optional): The flag whether the log should be repeatedly
+            parsed for the given patterns or not. Defaults to False.
+
+    Returns:
+        dict or list of dicts: The dictionary mapping pattern name to the parsed
+            group or list of groups. If repeat is True, a list of such
+            dictionaries will be returned.
+    """
     # load log file
     with open(path) as f:
         logs = f.read()
